86db01395aee90e7a97a74847d27b0a6c6d3f50a
Lady Jane <sweet.jane.s@outlook.com>
2026-03-10 16:45:20 -0300

    Made springboot project for backend

---
+++ backend/Apex-Health-Backend/src/test/java/com/nscc/apexhealthbackend/ApexHealthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nscc.apexhealthbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexHealthBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
